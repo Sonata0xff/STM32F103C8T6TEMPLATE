9aa42d8b6205dd2fc90cd7a3d7ca1e15631ab878
Sonata0xff <2080782895@qq.com>
2026-01-04 22:21:22 +0800

    oled driver process update

--- Drivers/BSP/inc/OLED.h
+++ Drivers/BSP/inc/OLED.h
@@ -8,7 +8,7 @@ void OLED_Init(); // mainly  focus on screen setting
 void OLED_TurnOn_Screen();
 void OLED_TurnOff_Screen();
 void OLED_Flash_Screen(unsigned char val);// flash the screen with value 'val'
-void OLED_Write1Byte(unsigned char v128, unsigned char v64, unsigned char val);// write a colunm
-void OLED_WriteIn_16x8Char(unsigned char v128, unsigned char v8, unsigned char val[]);// write a 16x8 size char
+void OLED_Write1Byte(unsigned char v128, unsigned char v8, unsigned char val);// write a colunm
+void OLED_WriteIn_16x8Char(unsigned char v128, unsigned char v8, unsigned char val);// write a 16x8 size char
 void OLED_WriteIn_16x8String(unsigned char v128, unsigned char v8, int len, unsigned char val[]); // write a string , each char's size is 16x8
 #endif

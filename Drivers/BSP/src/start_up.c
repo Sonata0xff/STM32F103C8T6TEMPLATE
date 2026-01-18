999c87ac05f0bf1955000a709a88e28ccd634ca3
Sonata0xff <2080782895@qq.com>
2026-01-18 19:35:28 +0800

    code clean

--- Drivers/BSP/src/start_up.c
+++ Drivers/BSP/src/start_up.c
@@ -2,26 +2,26 @@
 #ifdef START_UP_API_EN
 
 void SystemClock_Config()
-{
-    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-    // ??HSE????(8MHz)-> PLL???72MHz
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-    RCC_OscInitStruct.HSEState = RCC_HSE_ON;  // ????????
-    RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;  // 8MHz * 9 = 72MHz
+{	
+		RCC_OscInitTypeDef RCC_OscInitStruct = {
+			.OscillatorType = RCC_OSCILLATORTYPE_HSE,
+			.HSEState = RCC_HSE_ON,
+			.HSIState = RCC_HSI_OFF,
+			.PLL.PLLState = RCC_PLL_ON,
+			.PLL.PLLSource = RCC_PLLSOURCE_HSE,
+			.PLL.PLLMUL = RCC_PLL_MUL9// 8MHz * 9 = 72MHz
+		};
+		
+		RCC_ClkInitTypeDef RCC_ClkInitStruct = {
+			.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2,
+			.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK,
+			.AHBCLKDivider = RCC_SYSCLK_DIV1, // HCLK = 72MHz
+			.APB1CLKDivider = RCC_HCLK_DIV2,	// PCLK1 = 36MHz
+			.APB2CLKDivider = RCC_HCLK_DIV1		// PCLK2 = 72MHz
+		};
+		
     HAL_RCC_OscConfig(&RCC_OscInitStruct);
-
-    // ???????
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;    // HCLK = 72MHz
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;     // PCLK1 = 36MHz
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;     // PCLK2 = 72MHz (SPI1????)
-    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
+		HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
 }
 
 #endif

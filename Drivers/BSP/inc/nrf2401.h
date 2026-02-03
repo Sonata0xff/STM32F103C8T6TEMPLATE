ca34b54bdf7203cddcfcb86f91697d081598b404
Sonata0xff <2080782895@qq.com>
2026-02-04 00:37:21 +0800

    add code

--- Drivers/BSP/inc/nrf2401.h
+++ Drivers/BSP/inc/nrf2401.h
@@ -12,6 +12,15 @@
 Pip1 for data receive
 Pip0 for data Ack
 */
+
+enum CommStatus {
+	CommStatus_Idle = 0,
+	CommStatus_Standby,
+	CommStatus_Send,
+	CommStatus_Receive,
+	CommStatus_Shutdown
+};
+
 void NRF2401_Init(int CS_Line);
 
 //Start nrf2401, transport into standby-I mode.
@@ -20,13 +29,18 @@ void NRF2401_Start();
 //stop nrf2401, transport into shutdown mode.
 void NRF2401_Stop();
 
+//revert send / receive into standby-I
+void NRF2401_Revert_Standby();
+
 //transport to Send mode
 void NRF2401_Send_Mode();
-void NRF2401_Send(unsigned char* datas, unsigned char size);
+void NRF2401_Send(unsigned char* datas);
+void NRF2401_Send_FinHandle();
 
 //transport to Receive mode
 void NRF2401_Recv_Mode();
-void NRF2401_Recv();
+void NRF2401_Recv(unsigned char* datas);
+void NRF2401_Recv_FinHandle();
 
 void NRF2401_IRQ_Handler();
 #endif
--- Drivers/BSP/src/nrf2401.c
+++ Drivers/BSP/src/nrf2401.c
@@ -7,7 +7,7 @@
 static int CSN_ORDER = 0;
 unsigned char comm_addr[3] = {0xaa, 0xfe, 0xaa};//wait for check
 static int data_len = 2;//Bytes
-
+enum CommStatus comm_status = CommStatus_Idle; //init value
 AtomVarType NRF_SEND;//RESET means send finish 
 
 void NRF2401_Init(int CS_Line)
@@ -26,6 +26,7 @@ void NRF2401_Init(int CS_Line)
 	//init CSN
 	CSN_ORDER = CS_Line;
 	HAL_Delay(100);
+	comm_status = CommStatus_Shutdown;
 }
 
 void NRF2401_Start()
@@ -66,8 +67,10 @@ void NRF2401_Start()
 	
 	HAL_Delay(2);
 	
+	comm_status = CommStatus_Standby;
+	
 	//test func
-	orders[0] = 0x00;
+	/*orders[0] = 0x00;
 	orders[1] = 0x00;
 	ISPI1_SelectDevice(CSN_ORDER);
 	ISPI1_SenRecBytes(orders, recData, 2);
@@ -77,7 +80,7 @@ void NRF2401_Start()
 	for (int i = 0; i < 2; i++) {
 		TransNum2String(recData[i], tmp);
 		OLED_WriteIn_16x8String(i*4, 0, 4, (unsigned char*)tmp);
-	}
+	}*/
 }
 
 void NRF2401_Stop()
@@ -103,8 +106,12 @@ void NRF2401_Stop()
 	ISPI_Comm_Block_Wait();
 	ISPI1_UnSelectDevice(CSN_ORDER);
 	
+	//Change status
+	comm_status = CommStatus_Shutdown;
+	
+	
 	//test func
-	orders[0] = 0x00;
+	/*orders[0] = 0x00;
 	orders[1] = 0x00;
 	ISPI1_SelectDevice(CSN_ORDER);
 	ISPI1_SenRecBytes(orders, recData, 2);
@@ -114,7 +121,7 @@ void NRF2401_Stop()
 	for (int i = 0; i < 2; i++) {
 		TransNum2String(recData[i], tmp);
 		OLED_WriteIn_16x8String(i*4, 0, 4, (unsigned char*)tmp);
-	}
+	}*/
 }
 
 void NRF2401_Send_Mode()
@@ -162,18 +169,38 @@ void NRF2401_Send_Mode()
 	//init atom var
 	Atom_Write(&NRF_SEND, ATOM_VALUE_RESET);
 	
+	//transport into send mode
+	comm_status = CommStatus_Send;
+	//HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET); //CE = 1
+	
+}
+
+void NRF2401_Send(unsigned char* datas)
+{
+	//orders
+	unsigned char orders[data_len + 1];
 	//flash tx cache
 	orders[0] = 0xe1;
 	ISPI1_SelectDevice(CSN_ORDER);
 	ISPI1_SendBytes(orders, 1);
 	ISPI_Comm_Block_Wait();
 	ISPI1_UnSelectDevice(CSN_ORDER);
-	
-	//transport into send mode
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET); //CE = 1
+	//atom set
+	Atom_Write(&NRF_SEND, ATOM_VALUE_SET);
+	//write in data
+	orders[0] = 0xa0;
+	for (int i = 0; i < data_len; ++i) {
+		orders[i + 1] = datas[i];
+	}
+	ISPI1_SelectDevice(CSN_ORDER);
+	ISPI1_SendBytes(orders, data_len + 1);
+	ISPI_Comm_Block_Wait();
+	ISPI1_UnSelectDevice(CSN_ORDER);
+	//CE = 1, launch!
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);
 }
 
-void NRF2401_Send(unsigned char* datas, unsigned char size)
+void NRF2401_Send_FinHandle()
 {
 	//wait for coding ...
 }
@@ -184,7 +211,17 @@ void NRF2401_Recv_Mode()
 	
 }
 
-void NRF2401_Recv()
+void NRF2401_Recv(unsigned char* datas)
+{
+	//wait for coding ...
+}
+
+void NRF2401_Recv_FinHandle()
+{
+	//wait for coding ...
+}
+
+void NRF2401_Revert_Standby()
 {
 	//wait for coding ...
 }

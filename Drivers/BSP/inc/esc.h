518111b88339ec6a1f97283fa945eae0241be4f6
Sonata0xff <2080782895@qq.com>
2026-07-13 06:38:14 +0800

    esc dev

--- Drivers/BSP/inc/esc.h
+++ Drivers/BSP/inc/esc.h
@@ -1,8 +1,30 @@
-#include "stm32f1xx.h"                  // Device header
-#include "stm32f1xx_hal.h"
-#include "NSCP.h"
 #include "bsp_config.h"
 
 #ifdef ESC_API_EN
+#ifndef ESC_H
+#define ESC_H
 
+#include "stm32f1xx.h"                  // Device header
+#include "stm32f1xx_hal.h"
+#include "NSCP.h"
+
+typedef enum {
+	ESC_STATUS_IDLE,
+	ESC_STATIS_LOW_TRANS,
+	ESC_STATUS_LOW_STAB,
+	ESC_STATUS_TRANS
+} ESC_Status;
+
+typedef enum {
+	ESC_ERR_NERR,
+} ESC_ERROR;
+
+//esc app enterance.
+ESC_ERROR ESC_APP_Main();
+
+#ifdef ESC_DEBUG_MODE
+ESC_ERROR ESC_TRANS_LOW_STAB_Check_func();
+#endif
+
+#endif
 #endif
--- Drivers/BSP/src/esc.c
+++ Drivers/BSP/src/esc.c
@@ -1,4 +1,30 @@
 #include "esc.h"
 #ifdef ESC_API_EN
 
+//---------------private func--------------
+//power start running func.
+void ESC_APP_On()
+{
+	
+}
+//app env init. 
+void ESC_APP_Init()
+{
+	
+}
+
+
+//---------------public func---------------
+ESC_ERROR ESC_APP_Main()
+{
+	return ESC_ERR_NERR;
+}
+
+//---------------debug func----------------
+#ifdef ESC_DEBUG_MODE
+__weak ESC_ERROR ESC_TRANS_LOW_STAB_Check_func()
+{
+	return ESC_ERR_NERR;
+}
+#endif
 #endif
